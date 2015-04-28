658867e2b16386066e3d263c458e3ccedc18a7b3
xiaolukuan <xiaolukuan@chunyu.me>
2015-04-28 11:08:56 +0800

    first

---
+++ alex/build/source/buildConfig/debug/com/example/alex/BuildConfig.java
@@ -0,0 +1,7 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.alex;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+
+}
---
+++ alex/src/main/java/com/example/alex/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.alex;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(this,"alex" , 0).show();
+    }
+
+
+
+    
+}
